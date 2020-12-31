9820592297c9345793ee4b446b92f66532ec0a0f
Chris Hanson <hanson4288@gmail.com>
2020-12-31 10:53:49 -0600

    Parse URLs.

--- minimalist_baker_parser.go
+++ minimalist_baker_parser.go
@@ -1,5 +1,27 @@
 package recipe_parser
 
-func Hello() string {
-	return "Hello"
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+type HostNameNotMinimalistBakerError string
+
+func (h HostNameNotMinimalistBakerError) Error() string {
+	return fmt.Sprintf("Host name was not minimalistbaker.com: %q", string(h))
+}
+
+func ParseUrl(raw_url string) (*url.URL, error) {
+	recipe_url, err := url.Parse(raw_url)
+	if err != nil {
+		panic(err)
+	}
+	hostname := getHostName(recipe_url)
+	return recipe_url, HostNameNotMinimalistBakerError(hostname)
+}
+
+func getHostName(recipe_url *url.URL) string {
+	hostname_parts := strings.Split(recipe_url.Hostname(), ".")
+	return strings.Join(hostname_parts[len(hostname_parts)-2:], ".")
 }
--- minimalist_baker_parser_test.go
+++ minimalist_baker_parser_test.go
@@ -1,6 +1,8 @@
 package recipe_parser
 
-import "testing"
+import (
+	"testing"
+)
 
 func assertStringsEqual(t *testing.T, actual, expected string) {
 	t.Helper()
@@ -9,10 +11,40 @@ func assertStringsEqual(t *testing.T, actual, expected string) {
 	}
 }
 
-func TestMinimalistBakerParser(t *testing.T) {
-	t.Run("Test Hello", func(t *testing.T) {
-		actual := Hello()
-		expected := "Hello"
-		assertStringsEqual(t, actual, expected)
+func assertErrorsEqual(t testing.TB, actual error, expected HostNameNotMinimalistBakerError) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("Got %q but expected %q", actual, expected)
+	}
+}
+
+func TestMinimalistBakerUrlParser(t *testing.T) {
+	t.Run("Test panic if the URL isn't a valid URL.", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("The code did not panic.")
+			}
+		}()
+
+		_, err := ParseUrl("not_a_url")
+		if err != nil {
+			t.Errorf("Error was returned unexpectedly.")
+		}
+	})
+
+	t.Run("Test parse a URL with the wrong hostname.", func(t *testing.T) {
+		_, err := ParseUrl("https://www.epicurious.com/some/recipe/")
+		expectedErr := HostNameNotMinimalistBakerError("epicurious.com")
+		assertErrorsEqual(t, err, expectedErr)
+	})
+
+	t.Run("Test parse a URL with the correct hostname.", func(t *testing.T) {
+		url, _ := ParseUrl("https://minimalistbaker.com/easy-vegan-fried-rice/")
+
+		actualHostname := url.Hostname()
+		assertStringsEqual(t, actualHostname, "minimalistbaker.com")
+
+		actualPath := url.Path
+		assertStringsEqual(t, actualPath, "/easy-vegan-fried-rice/")
 	})
 }
